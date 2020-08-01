68be266d88290fc1668ac8e5b3c92b22bd59943f
Chengbin Huang <hcb12586@163.com>
2020-07-31 21:38:17 -0400

    update

---
+++ client/src/components/tours/Edit.jsx
@@ -0,0 +1,129 @@
+import React, { useState, useEffect } from 'react';
+import { Form, Container } from 'react-bootstrap';
+import Axios from 'axios';
+import { Redirect } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+
+
+const Edit = function (props) {
+
+  const id = props.location.state.id;
+
+  const [inputs, setInputs] = useState({
+    destination: '',
+    price: '',
+    status: 'available'
+  });
+
+  const [redirect, setRedirect] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const blogResp = await Axios.get(`/api/blogs/${id}`);
+      if (blogResp.status === 200) setInputs(blogResp.data);
+    })();
+  }, []);
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+
+    try {
+      const resp = await Axios.post('/api/blogs/update', inputs);
+
+      if (resp.status === 200)  {
+        toast("The blog was updated successfully", {
+          type: toast.TYPE.SUCCESS
+        });
+        setRedirect(true);
+      } else {
+        toast("There was an issue updating the blog", {
+          type: toast.TYPE.ERROR
+        });
+      }
+    } catch (error) {
+      toast("There was an issue updating the blog", {
+        type: toast.TYPE.ERROR
+      });
+    }
+  };
+
+  const handleInputChange = async event => {
+    event.persist();
+
+    const { name, value } = event.target;
+
+    setInputs(inputs => ({
+      ...inputs,
+      [name]: value
+    }));
+  };
+
+  if (redirect) return (<Redirect to="/tours"/>);
+
+  return (
+    <Container className="my-5">
+      <header>
+        <h1>Edit Tour</h1>
+      </header>
+
+      <hr/>
+
+      <div>
+        <Form onClick={handleSubmit}>
+          <Form.Group>
+            <Form.Label>destination:</Form.Label>
+            <Form.Control
+              name="destination"
+              onChange={handleInputChange}
+              value={inputs.destination}
+            />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label>Tour Type:</Form.Label>
+            <Form.Control
+              as="select"
+              name="tourType"
+              onChange={handleInputChange}
+              defaultValue={inputs.tourType}
+            >
+              {tourTypes.map((type, i) => (
+                <option key={i} value={type}>{type}</option>
+              ))}
+            </Form.Control>
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label>Group Size:</Form.Label>
+            <Form.Control
+              type="number"
+              name="groupSize"
+              step="1"
+              min="1"
+              max="10"
+              onChange={handleInputChange}
+              value={inputs.groupSize}
+            />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label>Date:</Form.Label>
+            <Form.Control
+              type="date"
+              name="date"
+              onChange={handleInputChange}
+              value={inputs.date}
+            />
+          </Form.Group>
+
+          <Form.Group>
+            <button type="submit" className="btn btn-primary">Update</button>
+          </Form.Group>
+        </Form>
+      </div>
+    </Container>
+  );
+};
+
+export default Edit;
